a7a264399a6432f14f32194d75f0fb34f8e9112f
chufeng <chufeng@users.noreply.github.com>
2024-03-06 13:59:01 +0800

    更新网址

--- site.config.ts
+++ site.config.ts
@@ -1,7 +1,7 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://valaxy.site/',
+  url: 'https://chufenglizan.vercel.app/',
   lang: 'zh-CN',
   title: '楚风',
   author: {
