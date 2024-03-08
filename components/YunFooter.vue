f46392c46ebd46a00d5dc738cc81e1fbdd6cd8ea
chufeng <chufeng@users.noreply.github.com>
2024-03-08 19:37:09 +0800

    增加运行时间

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -10,10 +10,11 @@ useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
     <a href="https://icp.gov.moe/?keyword=20246351" target="_blank">萌ICP备20246351号</a>
     <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
     <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
+
     <VCLiveTime mt="2" start-time="2023-12-12">
-      <!-- <template #live-time-before>
-        <span>本站已运行</span>
-      </template> -->
+      <template #live-time-before>
+        <span>本站已稳定运行</span>
+      </template>
     </VCLiveTime>
   </YunFooter>
 </template>
