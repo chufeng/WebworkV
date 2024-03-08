c529cf6093d93333cfb8ee4fbe93fcda810f319d
chufeng <chufeng@users.noreply.github.com>
2024-03-08 19:56:47 +0800

    更新 div 的 id

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -8,8 +8,8 @@ useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
   <YunFooter>
     <!-- 自定义页脚内容 -->
     <a href="https://icp.gov.moe/?keyword=20246351" target="_blank">萌ICP备20246351号</a>
-    <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
-    <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
+    <div id="3">本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
+    <div id=‘2’>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
 
     <VCLiveTime mt="2" start-time="2023-12-12">
       <template #live-time-before>
